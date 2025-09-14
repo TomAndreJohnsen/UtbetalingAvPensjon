9a3f0b6d988c450f8f5928cde8e4ff321e526aa9
TomAndreJohnsen <tomandre@Toms-MacBook-Pro.local>
2025-09-14 14:35:45 +0200

    Maanedlig_saldo funksjon i pensjon.py

---
+++ kalkulator/pensjon.py
@@ -0,0 +1,9 @@
+def maanedlig_saldo(startbelop, maander, aarlig_rente, maanedlig_uttak):
+    saldo_liste = []
+    saldo = startbelop
+    for mnd in range(maander):
+        saldo += saldo * (aarlig_rente/12)
+        saldo -= maanedlig_uttak
+        saldo_liste.append(saldo)
+    return saldo_liste
+
