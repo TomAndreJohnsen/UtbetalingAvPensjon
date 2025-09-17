1c0e8bcaf623bf2007d5ed92b66853b90678b7c2
TomAndreJohnsen <tomandre@Toms-MacBook-Pro.local>
2025-09-17 17:11:44 +0200

    Print for totalverdi og total formueskatt

--- main.py
+++ main.py
@@ -5,8 +5,11 @@ from kalkulator.parametere import RenteScenario
 
 formueskatt_list = []
 
+
 param = Parametere()
 slutt, formueskatt_list = sluttverdi_ved_start(param)
 
 print("Fondets verdi ved start av pensjon: ", f"{round(slutt, 2):,}".replace(",", "."))
+formueskatt_totalt = sum(formueskatt_list)
+print("Total av formueskatt: ", f"{round(formueskatt_totalt, 2):,}".replace(",", "."))
 #print(formueskatt_list)
